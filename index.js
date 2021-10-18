64756ffbf3bba61271ad02b58c8351b356b59d1c
sshusshu <subin9033@gmail.com>
2021-10-18 17:07:43 +0900

    boilerplate ...ing

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/Users");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://subin:rmeodu11!!@cluster0.dc6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect("mongodb+srv://subin:rmeodu11!!@cluster0.dc6i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
   .then(() => console.log("mongoDB connected"))
   .catch((err) => console.log(`error!!! ${err}`));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// app.post("/register", (req, res) => {
+//   //client의 회원가입 정보를 db에 넣기
+//   const user = new User(req.body);
+//   user.save((err, userInfo) => {
+//     if (err) return res.json({ success: false, err });
+//     return res.status(200).json({ success: true });
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
