188b3773f348dc12a659642146f4ba0408cc1c3f
subin <subin9033@gmail.com>
2021-10-23 10:45:23 +0900

    BUILD: 회원가입

---
+++ config/key.js
@@ -0,0 +1,5 @@
+if (process.send.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://sshusshu:rmeodu11!!@cluster0.gemb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-app.get("/", (req, res) => res.send("hello world!"));
+app.get("/", (req, res) => res.send("hello world!!!!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
-//mongodb+srv://sshusshu:<password>@cluster0.gemb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritys
+const { User } = require("./models/Users");
+app.post("/register", (req, res) => {
+  // 회원 가입 시 필요한 정보를 client에서 가져와서 데이터베이스에 입력
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
