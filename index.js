b84be838dea548d6cd50925d1141c334a662c30c
subin <subin9033@gmail.com>
2021-10-23 22:22:31 +0900

    ADD : login,logout

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -26,7 +28,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   //요청된 이메일을 데이터베이스에서 검색
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -35,15 +37,42 @@ app.post("/login", (req, res) => {
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
-  });
-  //비밀번호가 맞는지 확인
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if (isMatch)
-      return res.json({
-        loginSuccess: false,
-        message: "비밀번호가 틀렸습니다.",
+    //비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      //토큰 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
       });
-    //토큰 생성
-    user.generateToken((err) => {});
+    });
+  });
+});
+
+const { auth } = require("./middleware/auth");
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
   });
 });
---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const { User } = require("../models/Users");
+
+let auth = (req, res, next) => {
+  //인증 처리
+  //client 쿠키에서 토큰을 가져옴
+  let token = req.cookies.x_auth;
+  //토큰 복호화 후 유저 검색
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+    if (!user) return res.json({ isAuth: false, error: true });
+    req.token = token;
+    req.user = user;
+    next();
+  });
+  //유저가 있으면 인증 O
+  //유저가 없으면 인증 X
+};
+module.exports = { auth };
