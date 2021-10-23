ff815561c17a9efb6f6527adf6ba61534bac184e
subin <subin9033@gmail.com>
2021-10-23 16:56:25 +0900

    Add: login 기능 추가

--- index.js
+++ index.js
@@ -16,11 +16,34 @@ app.get("/", (req, res) => res.send("hello world!!!!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
 const { User } = require("./models/Users");
+
 app.post("/register", (req, res) => {
   // 회원 가입 시 필요한 정보를 client에서 가져와서 데이터베이스에 입력
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 검색
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+  });
+  //비밀번호가 맞는지 확인
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if (isMatch)
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다.",
+      });
+    //토큰 생성
+    user.generateToken((err) => {});
   });
 });
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: { type: String, maxlength: 50 },
@@ -10,6 +12,30 @@ const userSchema = mongoose.Schema({
   token: { type: String },
   tokenExp: { type: Number },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  // 비밀번호 암호화
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
