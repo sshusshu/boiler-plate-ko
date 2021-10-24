426ea1527b01eff7eccf2f911a334c6834fd3a12
subin <subin9033@gmail.com>
2021-10-24 21:32:58 +0900

    ADD : server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage}></Route>
+          <Route exact path="/login" component={LoginPage}></Route>
+          <Route exact path="/register" component={RegisterPage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("./server/config/key");
+const config = require("./config/key");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,9 +15,14 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 app.get("/", (req, res) => res.send("hello world!!!!"));
+
+app.get("/api/hello", (req, res) => {
+  res.send("안녕");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
-const { User } = require("./models/Users");
+const { User } = require("./");
 
 app.post("/register", (req, res) => {
   // 회원 가입 시 필요한 정보를 client에서 가져와서 데이터베이스에 입력
